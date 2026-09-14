d71f8530e14ffc9615012c8605f3d4812d245f1b
Oluebube Ogbonnaya <oluebubeogbonnaya@gmail.com>
2026-09-14 06:04:21 +0100

    mova now supports fontawesome icon usage

--- mova-themes/default/layout.php
+++ mova-themes/default/layout.php
@@ -81,9 +81,8 @@ if (isset($seo) && is_object($seo)) {
     <?php if ($faviconUrl): ?>
         <link rel="icon" href="<?= htmlspecialchars($faviconUrl) ?>">
     <?php endif; ?>
-<?php if ($needFontAwesome): ?>
+    <!-- Font Awesome included by default so icons like fa-users, fa-solid, etc. work out of the box -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer">
-    <?php endif; ?>
     <link rel="stylesheet" href="/assets/css/mova.css">
 
     <?php
